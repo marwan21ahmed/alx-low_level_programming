5c3d1ea003fd1a809caf7b09adf383233f2ed6ea
Marwan <marwan@example.com>
2023-07-15 05:23:45 -0700

    z

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ 
+ * Description : A C  program that prints with put function
+ *
+ * Return: Always 0 (Sucess)
+*/
+iint main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle ");
+	return (0);
+}
